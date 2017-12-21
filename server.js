af8f4f3107a47457a320e8e547d315e772415283
warrend <warrend555@gmail.com>
2017-12-21 17:28:05 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+var fs = require('fs');
+const path = require('path') 
+const open = require('open')
+const app = express();  
+const port = 8000;  
+
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(port, function(err) {  
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+    console.log("Server is running on port + " + port)
+  }
+});
